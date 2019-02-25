59e9c14f04ec798a7c7c2b9733a65ff439987637
johannes <johannes@auterion.com>
2019-02-25 15:57:16 +0100

    add setup.py and ignore package build files

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Library for ecl ekf analysis.
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+CLASSIFIERS = """\
+Development Status :: 1 - Planning
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Other
+Topic :: Software Development
+Topic :: Scientific/Engineering :: Artificial Intelligence
+Topic :: Scientific/Engineering :: Mathematics
+Topic :: Scientific/Engineering :: Physics
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
+Operating System :: MacOS
+"""
+
+# pylint: disable=invalid-name
+
+setup(
+    name='ecl-ekf-analysis',
+    maintainer="Daniel Agar",
+    maintainer_email="daniel@px4.io",
+    description="A library for PX4 ecl ekf analysis.",
+    long_description=long_description,
+    url='https://github.com/PX4/ecl-ekf-analysis',
+    author='Johannes Brand',
+    author_email='johannes@auterion.com',
+    download_url='https://github.com/PX4/ecl-ekf-analysis',
+    license='BSD 3-Clause',
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    install_requires=['pyulog', 'matplotlib'],
+    tests_require=['pytest'],
+    test_suite='pytest',
+    packages=find_packages(),
+    version="0.01",
+)
